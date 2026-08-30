5270a5382c9033ef89fd6d3325d452f8c2cb7c0a
KaiToyama <toyamakai2002@gmail.com>
2026-08-30 20:41:03 +0900

    unityクライアントからapiを叩けるかのテスト

---
+++ Client/DrawingSocialGame/Assets/Script/Network/ApiTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class  ApiTest : MonoBehaviour
+{
+    private const string ApiUrl = "http://127.0.0.1:8000/api/test";
+
+    private void Start()
+    {
+        StartCoroutine(TestApi());
+    }
+
+    private IEnumerator TestApi()
+    {
+        using UnityWebRequest request = UnityWebRequest.Get(ApiUrl);
+
+        yield return request.SendWebRequest();
+
+        if(request.result == UnityWebRequest.Result.Success)
+        {
+            Debug.Log($"API Response: {request.downloadHandler.text}");
+        }
+        else
+        {
+            Debug.LogError($"API Request Error: {request.error}");
+        }
+    }
+}
